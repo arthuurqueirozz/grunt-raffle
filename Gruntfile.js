8eba88009cfc70edad3a96e0aab638551c4e1c64
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-04-29 18:29:37 -0300

    adding replace plugin for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,26 @@ module.exports = function(grunt) {
             less: {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'CSS_ADDRESS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'src/index.html',
+                        dest: 'dev/'
+                    }
+                ]
             }
         }
     });
@@ -27,6 +47,7 @@ module.exports = function(grunt) {
     
     grunt.loadNpmTasks('grunt-contrib-less')
     grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
 
     grunt.registerTask('default', ['watch'])
     grunt.registerTask('build', ['less:production'])
